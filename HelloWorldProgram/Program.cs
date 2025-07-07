afa12d400b106daec6b347c8926473f5f2d74e23
ChrisToky <rezacolomb@gmail.com>
2025-07-07 09:17:46 -0400

    Add project files.

---
+++ HelloWorldProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿//  This is traditionally the first program written.
+using System;
+using static System.Console;
+
+namespace HelloWorldProgram
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            WriteLine("Hello World!");
+            WriteLine();
+
+            ReadKey();
+            WriteLine("this is a statement");
+
+            WriteLine();
+
+            WriteLine("Addition: " + 5 + 6);
+        }
+    }
+}
